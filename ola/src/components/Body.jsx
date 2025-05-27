8aa5727be622ff72800c11497e737d741f288478
rafaellindemann <rafaelld@gmail.com>
2025-05-26 22:19:23 -0300

    add ola react tiinf

---
+++ ola/src/components/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Body.css'
+function Body() {
+  return (
+    <div className='Body'>
+        <h2 className='titulo2'>Dinos são legais</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus pariatur, facere maxime beatae vitae amet qui omnis ut perferendis. Tempore voluptatum magni ullam sint et dignissimos? Praesentium molestias dicta quidem.</p>
+    </div>
+  )
+}
+
+export default Body
